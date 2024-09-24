f8343cae7eb364918167d7d64c700ed6a790d946
Juman Kaushik <juman.code@gmail.com>
2024-09-24 18:49:10 +0530

    added the basic layout with all the component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {Routes, Route, Link} from 'react-router-dom'
+import {Layout, Typography, Space} from 'antd'
 import './App.css'
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, CryptoDetails, News } from './components'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <h1>Crypto App</h1>
+    <div className="app">
+      <div className="navbar">
+          <Navbar/>
+      </div>
+      <div className="main">
+          <Layout>
+              <div className="routes">
+                  <Routes>
+                      <Route exact path='/' element={<Homepage/>} />
+                      <Route exact path='/exchanges' element={<Exchanges/>} />
+                      <Route exact path='/cryptocurrencies' element={<Cryptocurrencies/>} />
+                      <Route exact path='/crypto/:coinId' element={<CryptoDetails/>} />
+                      <Route exact path='/news' element={<News/>} />
+                  </Routes>
+              </div>
+          </Layout>
+          <div className="footer">
+              <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}} >
+                Cryptoverse <br />
+                All rights reserved
+              </Typography.Title>
+              <Space>
+                <Link to="/">Home</Link>
+                <Link to="/exchanges">Exchanges</Link>
+                <Link to="/news">News</Link>
+              </Space>
+        </div>
+      </div>
+    </div>
   )
 }
 

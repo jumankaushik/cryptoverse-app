b30c5e75b344b5825d6884c544e4afc3efdfca73
Juman Kaushik <juman.code@gmail.com>
2024-09-25 21:29:23 +0530

    news page added 🗞️

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,26 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+
+const cryptoNewsHeaders = {
+    'x-rapidapi-key': '49a5784214msh997f185d7d8bb3fp100bb8jsn804a679df940',
+    'x-rapidapi-host': 'google-news22.p.rapidapi.com'
+  }
+
+
+const baseUrl = 'https://google-news22.p.rapidapi.com/v1'
+
+const createRequest = (url) => ({
+    url, headers: cryptoNewsHeaders
+})
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) =>({
+        getCryptoNews: builder.query({
+            query: (newsCategory) => createRequest(`/search?q=${newsCategory}&country=us&language=en`)
+        })
+    })
+})
+
+export const { useGetCryptoNewsQuery } = cryptoNewsApi;

8f4e683e43f826c9dbae9063ea616e2f626e3a18
Juman Kaushik <juman.code@gmail.com>
2024-09-26 16:38:00 +0530

    Navbar responsive for smaller screen 🥰

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Row, Typography, Col } from "antd";
 
+import BgImage from "../images/full-stack.png";
+
+// Exchanges (Commin Soon)
 const Exchanges = () => {
   return (
-    <div>Exchanges</div>
-  )
-}
+    <Col gutter={[32, 32]} className="exchanges-card-container">
+      <img src={BgImage} alt="Coming Soon" className="exchanges-image" loading="lazy" />
+      <Typography.Title level={2} className="exchanges-heading">
+        Coming Soon
+      </Typography.Title>
+    </Col>
+  );
+};
 
-export default Exchanges
+export default Exchanges;
